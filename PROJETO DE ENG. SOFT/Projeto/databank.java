1fbf7c3953bd5436669467bd7691b4f95ea83f86
Guto <165092077+20241tadssaj0030@users.noreply.github.com>
2025-02-06 18:28:35 -0300

    Add files via upload

---
+++ PROJETO DE ENG. SOFT/Projeto/databank.java
@@ -0,0 +1,61 @@
+public abstract class databank{
+    
+    //Informações padrão
+    protected int idresp; //Id para quantidade de respostas
+    protected int ididade; //Id para armazenar idades
+    //Gênero
+    protected int idmasc; //Id para masculino
+    protected int idfem; //Id para feminino
+    protected int idtrans; //Id para transgênero
+    protected int idnbi; //Id para não binário
+    protected int idgen; //Id para outro gênero
+    //Raça-Cor
+    protected int idn; //Id de cor (quantidade de pessoas negras)
+    protected int idb; //Id de cor (quantidade de pessoas brancas)
+    protected int idp; //Id de cor (quantidade de pessoas pardas)
+    protected int idpo; //Id de cor (quantidades de povos originários)
+    //Escolaridade
+    protected int idemi; //Id para ensino médio incompleto
+    protected int idemc; //Id para ensino médio completo
+    protected int idgi; //Id para graduação incompleta
+    protected int idgc; //Id para graduação completa
+    protected int idefi; //Id para ensino fundamental incompleto
+    protected int idefc; //Id para ensino fundamental completo
+    protected int idescnda; //Id para os que preferem não informar a escolaridade
+    //Estado civil
+    protected int idcas; //Id para casados
+    protected int idviuv; //Id para viuvos
+    protected int iddiv; //Id para divorciados
+    protected int idsep; //Id para separados
+    protected int idsolt; //Id para solteiros
+    protected int idecnda; //Id para os que preferem não informar o estado civil
+    //Emprego
+    protected int idmp; //Id para os que trabalham meio período
+    protected int iddesemp; //Id para os que estão desempregados
+    protected int idincap; //Id para os incapazes de trabalhar
+    protected int idapos; //Id para os aposentados
+    protected int idempnda; //Id para os que preferem não informar o emprego
+    //Auxílios
+    protected int idbf; //Id para os que tem bolsa família
+    protected int idaux; //Id para "outros auxilios, quais?" 
+    //-------------------------------------Falta fazer a parte dos "quais auxilios"-------------------------------------// 
+    //Família e dependentes
+    protected int idnone; //Id para os que não possuem nenhum familiar/dependente
+    protected int idone; //Id para os que possuem apenas um familiar/dependente
+    protected int idtwo; //Id para os que possuem dois familiares/dependentes
+    protected int idthree; //Id para os que possuem três familiares/dependentes
+    protected int idfour; //Id para os que possuem quatro famiiares/dependentes
+    protected int idfive; //Id para os que posuem cinco ou mais familiares/dependentes
+    protected int idnda; //Id para os que preferem não informar a quantidade de familiares/dependentes
+    //Religião
+    protected int idcat; //Id para os que são católicos
+    protected int ideva; //Id para os que são evangélicos
+    protected int idcand; //Id para os que são do candomblé
+    protected int idumb; //Id para os que são da umbanda
+    protected int idespi; //Id para os que são espíritas
+    protected int idrnda; //Id para os que não tem religião
+    protected int idrpni; //Id para os que preferem não informar a religião
+    protected int idro; //Id para "outra religião"
+    //--------------------------------------Falta fazer a parte do "qual religião"--------------------------------------//
+
+}    
