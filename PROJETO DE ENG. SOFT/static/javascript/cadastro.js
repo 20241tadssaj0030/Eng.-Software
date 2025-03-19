cb695106e9f22ce2b1a5ee77d8468a9efacff657
Guto <165092077+20241tadssaj0030@users.noreply.github.com>
2025-03-19 18:02:02 -0300

    Mudanças

--- PROJETO DE ENG. SOFT/static/javascript/cadastro.js
+++ PROJETO DE ENG. SOFT/static/javascript/cadastro.js
@@ -1,30 +1,5 @@
-
-
-
-const tipocadastro = document.querySelector("#tipocadastro");
-const titulomoderacao = document.querySelector("#titulomoderacao");
 const titulousuario = document.querySelector("#titulousuario");
-const moderacaocadastro = document.querySelector("#moderacaocadastro");
 const usuariocadastro = document.querySelector("#usuariocadastro");
-
-tipocadastro.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const selecionartipocadastro = document.querySelector("#selecionartipocadastro").value;
-    if (selecionartipocadastro === 'moderacao'){
-        titulomoderacao.style.display = 'block';
-        moderacaocadastro.style.display = 'block';
-        titulousuario.style.display = 'none';
-        usuariocadastro.style.display = 'none';
-        moderacaocadastro.scrollIntoView({behavior: 'smooth'})
-       
-    }else{
-        titulomoderacao.style.display = 'none';
-        moderacaocadastro.style.display = 'none';
-        titulousuario.style.display = 'block';
-        usuariocadastro.style.display = 'block';
-        usuariocadastro.scrollIntoView({behavior: 'smooth'})
-    }
-} )
 
 usuariocadastro.addEventListener('submit', (event) =>{
     event.preventDefault();
@@ -40,31 +15,8 @@ usuariocadastro.addEventListener('submit', (event) =>{
     }else{
         msgerrousuario.style.display = 'none';
         repetirsenha.style.background = 'white';
-        senha.style.background = 'white';        usuariocadastro.submit();
+        senha.style.background = 'white';        
+        usuariocadastro.submit();
         return true;
     }
-
-
-})
-
-moderacaocadastro.addEventListener('submit', (event) =>{
-    event.preventDefault();
-    const senha = document.querySelector("#senhamoderacao");
-    const repetirsenha = document.querySelector("#repetirsenhamoderacao");
-    const msgerro = document.querySelector("#msgerromoderacao");
-    
-    if (senha.value !== repetirsenha.value){
-        msgerromoderacao.style.display = 'block';
-        repetirsenha.style.background = 'red';
-        senha.style.background = 'red';
-        return false;
-    }else{
-        msgerrousuario.style.display = 'none';
-        repetirsenha.style.background = 'white';
-        senha.style.background = 'white';
-        moderacaocadastro.submit();
-        return true;
-    }
-
-    
 })
---
+++ PROJETO DE ENG. SOFT/static/javascript/questionario.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll(".section"); // Seleciona todas as divs com a classe 'section'
+    const buttons = document.querySelectorAll("button");
+
+    sections.forEach((section, index) => {
+        if (index !== 0) {
+            section.style.display = "none";
+        }
+    });
+
+    buttons.forEach((button, index) => {
+        button.addEventListener("click", function (event) {
+            if (button.type === "submit") {
+                event.preventDefault();
+            }
+
+            if (index < sections.length - 1) {
+                sections[index].style.display = "none";
+                sections[index + 1].style.display = "block";
+            }
+        });
+    });
+});
